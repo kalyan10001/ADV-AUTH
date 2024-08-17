3d0fdb38098d41c544b5765c8ed5fe2c2aacf352
kalyan10001 <venkatakalyan1000@gmail.com>
2024-08-17 10:21:13 +0530

    signup,verify email,logout completed

---
+++ backend/mailtrap/emails.js
@@ -0,0 +1,41 @@
+import { VERIFICATION_EMAIL_TEMPLATE } from "./emailTemplates.js";
+import { mailtrapClient, sender } from "./mailtrap.config.js";
+
+export const sendVerificationEmail=async(email,verificationToken)=>{
+    const recipent=[{email}];
+    try {
+        const response=await mailtrapClient.send({
+            from:sender,
+            to:recipent,
+            subject:"Verify Your Email",
+            html:VERIFICATION_EMAIL_TEMPLATE.replace("{verificationCode}",verificationToken),
+            category:"Email Verification",
+        })
+        console.log("email sent successfully",response);
+        
+    } catch (error) {
+        console.error(`error in sending verification`,error);
+        throw new Error(`error sending verification email:${error}`);
+    }
+}
+
+export const sendwelcomeEmail=async(email,name)=>{
+    const recipent=[{email}];
+
+    try {
+        const response=await mailtrapClient.send({
+            from:sender,
+            to:recipent,
+            template_uuid:"f2c392f4-31b0-490b-a10b-4fa48be20d29",
+            template_variables:{
+                company_info_name:"Auth Company",
+                name: name,
+            },
+        });
+        console.log("welcome email sent successfully",response);
+    } catch (error) {
+        console.log(`error sending welcome email`,error);
+        throw new Error(`error sending welcome email:${error}`);
+        
+    }
+}
