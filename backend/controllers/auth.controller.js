5a654f2837d84dc71e3c461793639b6335f96661
kalyan10001 <venkatakalyan1000@gmail.com>
2024-08-17 10:42:34 +0530

    login functionality implemented

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -83,7 +83,34 @@ export const verifyEmail=async(req,res)=>{
 }
 
 export const login=async(req,res)=>{
-    res.send("login route");
+    const {email,password}=req.body;
+	try {
+		const user=await User.findOne({email});
+		if(!user)
+		{
+			return res.status(400).json({success:false,message:"invalid credentials"});
+		}
+		const isPasswordValid=await bcryptjs.compare(password,user.password);
+		if(!isPasswordValid)
+			{
+				return res.status(400).json({success:false,message:"invalid credentials"});
+			}
+		generateTokenAndSetCookie(res,user._id);
+		
+		user.lastLogin=new Date();
+		await user.save();
+		res.status(200).json({
+			success:true,
+			message:"logged in successfully",
+			user:{
+				...user._doc,
+				password:undefined,
+			},
+		});
+	} catch (error) {
+		console.log("error in login",error);
+		res.status(400).json({success:false,message:error.message});
+	}
 }
 
 export const logout=async(req,res)=>{
