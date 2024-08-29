990eaf7fca964aee175db9e24746267f19abd9bf
kalyan10001 <venkatakalyan1000@gmail.com>
2024-08-29 10:37:37 +0530

    background framer-motion setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,13 @@
-
+import FloatingShape from "./components/FloatingShape.jsx"
 
 function App() {
-
   return (
-    <>
-      <h1 className="text-red-500 text-5xl">hello</h1>
-    </>
+    <div
+			className='min-h-screen bg-gradient-to-br
+    from-gray-900 via-green-900 to-emerald-900 flex items-center justify-center relative overflow-hidden'>
+      			<FloatingShape color='bg-green-500' size='w-64 h-64' top='-5%' left='10%' delay={0} />
+    </div>
   )
 }
 
-export default App
+export default App;
