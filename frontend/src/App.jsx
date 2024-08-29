3c1676c1045b115faa971ae32d85023bdd7d66fd
kalyan10001 <venkatakalyan1000@gmail.com>
2024-08-29 19:52:52 +0530

    login and signup implemented ui

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,7 @@
+import { Route, Routes } from "react-router-dom";
 import FloatingShape from "./components/FloatingShape.jsx"
+import SignUpPage from "./pages/SignUpPage.jsx";
+import LoginPage from "./pages/LoginPage.jsx";
 
 function App() {
   return (
@@ -6,6 +9,12 @@ function App() {
 			className='min-h-screen bg-gradient-to-br
     from-gray-900 via-green-900 to-emerald-900 flex items-center justify-center relative overflow-hidden'>
       			<FloatingShape color='bg-green-500' size='w-64 h-64' top='-5%' left='10%' delay={0} />
+
+            <Routes>
+              <Route path="/" element={"home"}/>
+              <Route path="/signup" element={<SignUpPage/>}/>
+              <Route path="/login" element={<LoginPage/>}/>
+            </Routes>
     </div>
   )
 }
