0c010571ebedf8c73d0af062255732186a08cf5e
kalyan10001 <venkatakalyan1000@gmail.com>
2024-09-09 21:08:55 +0530

    forgot password and reset password implemented

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,22 @@
 import { Navigate, Route, Routes } from "react-router-dom";
-import FloatingShape from "./components/FloatingShape.jsx"
+import FloatingShape from "./components/FloatingShape";
+
 import SignUpPage from "./pages/SignUpPage.jsx";
 import LoginPage from "./pages/LoginPage.jsx";
 import EmailVerificationPage from "./pages/EmailVerificationPage.jsx";
-import { Toaster } from "react-hot-toast";
-import { useAuthStore } from "./store/authStore.js";
-import DashBoardPage from "./pages/DashBoardPage.jsx";
-import { useEffect } from "react";
+import DashboardPage from "./pages/DashBoardPage.jsx";
+import ForgotPasswordPage from "./pages/ForgotPasswordPage.jsx";
+import ResetPasswordPage from "./pages/ResetPasswordPage.jsx";
+
 import LoadingSpinner from "./components/LoadingSpinner.jsx";
 
+import { Toaster } from "react-hot-toast";
+import { useEffect } from "react";
+import { useAuthStore } from "./store/authStore.js";
 
 const ProtectedRoute = ({ children }) => {
 	const { isAuthenticated, user } = useAuthStore();
+
 	if (!isAuthenticated) {
 		return <Navigate to='/login' replace />;
 	}
@@ -23,40 +28,72 @@ const ProtectedRoute = ({ children }) => {
 
 const RedirectAuthenticatedUser = ({ children }) => {
 	const { isAuthenticated, user } = useAuthStore();
-
 	if (isAuthenticated && user.isVerified) {
 		return <Navigate to='/' replace />;
 	}
-
 	return children;
 };
-function App() {
- 
-  const { isCheckingAuth, checkAuth ,isAuthenticated,user} = useAuthStore();
 
+function App() {
+	const { isCheckingAuth, checkAuth } = useAuthStore();
 	useEffect(() => {
 		checkAuth();
 	}, [checkAuth]);
-  console.log("isauthenticated",isAuthenticated);
-  console.log("ischecking auth",isCheckingAuth);
-  if(isCheckingAuth)
-	return <LoadingSpinner/>
-
-  return (
-    <div
+	if (isCheckingAuth) return <LoadingSpinner />;
+	return (
+		<div
 			className='min-h-screen bg-gradient-to-br
-    from-gray-900 via-blue-900 to-gray-500 flex items-center justify-center relative overflow-hidden'>
-      			<FloatingShape color='bg-blue-400' size='w-64 h-64' top='-5%' left='10%' delay={0} />
-
-            <Routes>
-              <Route path="/" element={<ProtectedRoute><DashBoardPage/></ProtectedRoute>}/>
-              <Route path="/signup" element={<RedirectAuthenticatedUser><SignUpPage/></RedirectAuthenticatedUser>}/>
-              <Route path="/login" element={<RedirectAuthenticatedUser><LoginPage/></RedirectAuthenticatedUser>}/>
-              <Route path="/verify-email" element={<EmailVerificationPage/>}/>
-            </Routes>
-            <Toaster/>
-    </div>
-  )
+    from-gray-900 via-blue-900 to-emerald-900 flex items-center justify-center relative overflow-hidden'
+		>
+			<FloatingShape color='bg-blue-500' size='w-64 h-64' top='-50%' left='10%' delay={0} />
+			<FloatingShape color='bg-lime-500' size='w-32 h-32' top='40%' left='-10%' delay={2} />
+			<Routes>
+				<Route
+					path='/'
+					element={
+						<ProtectedRoute>
+							<DashboardPage />
+						</ProtectedRoute>
+					}
+				/>
+				<Route
+					path='/signup'
+					element={
+						<RedirectAuthenticatedUser>
+							<SignUpPage />
+						</RedirectAuthenticatedUser>
+					}
+				/>
+				<Route
+					path='/login'
+					element={
+						<RedirectAuthenticatedUser>
+							<LoginPage />
+						</RedirectAuthenticatedUser>
+					}
+				/>
+				<Route path='/verify-email' element={<EmailVerificationPage />} />
+				<Route
+					path='/forgot-password'
+					element={
+						<RedirectAuthenticatedUser>
+							<ForgotPasswordPage />
+						</RedirectAuthenticatedUser>
+					}
+				/>
+				<Route
+					path='/reset-password/:token'
+					element={
+						<RedirectAuthenticatedUser>
+							<ResetPasswordPage />
+						</RedirectAuthenticatedUser>
+					}
+				/>
+				<Route path='*' element={<Navigate to='/' replace />} />
+			</Routes>
+			<Toaster />
+		</div>
+	);
 }
 
 export default App;
